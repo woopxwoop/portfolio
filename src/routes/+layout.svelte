105cb1296bce9e4a154e69138ff468161b8c1708
An Lou <louan1734@gmail.com>
2026-03-21 15:55:38 -0500

    z-index nav

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -57,7 +57,7 @@
 
 <div class="flex w-full flex-col items-center">
 	<nav
-		class="site-nav accent-font fixed z-10 flex w-full flex-row justify-start gap-4 p-4 px-16 backdrop-blur-sm"
+		class="site-nav accent-font fixed z-30 flex w-full flex-row justify-start gap-4 p-4 px-16 backdrop-blur-sm"
 	>
 		{#each navItems as navItem (navItem.path)}
 			<a
